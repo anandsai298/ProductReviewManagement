8045847bf40f5a62963768cd1ce601bb3ce2df4b
anandsai298 <anandsai298@gmail.com>
2023-04-24 14:11:46 +0530

    [sai] Add : Added the Product review details in alist

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ProductReviewManagement;
+class program
+{
+    static void Main(string[] args )
+    {
+      Console.WriteLine("ProductReviewManagement");
+        List<ProductDetails> list = new List<ProductDetails>();
+        list.Add( new ProductDetails { ProductID=1,UserID=1,Rating=5,Review="Good",IsLikeFields=true } );
+        list.Add(new ProductDetails { ProductID = 2, UserID = 1, Rating = 5, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 3, UserID = 1, Rating = 5, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 4, UserID = 2, Rating = 4, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 5, UserID = 1, Rating = 4, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 6, UserID = 3, Rating = 3, Review = "AVG", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 7, UserID = 1, Rating = 3, Review = "AVG", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 8, UserID = 4, Rating = 5, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 9, UserID = 3, Rating = 5, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 10, UserID = 2, Rating = 2, Review = "BELOW_AVG", IsLikeFields = false });
+        list.Add(new ProductDetails { ProductID = 11, UserID = 5, Rating = 2, Review = "BELOW_AVG", IsLikeFields = false });
+        list.Add(new ProductDetails { ProductID = 12, UserID = 6, Rating = 1, Review = "WORST", IsLikeFields = false });
+        list.Add(new ProductDetails { ProductID = 13, UserID = 7, Rating = 1, Review = "WORST", IsLikeFields = false });
+        list.Add(new ProductDetails { ProductID = 14, UserID = 4, Rating = 5, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 15, UserID = 5, Rating = 5, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 16, UserID = 8, Rating = 5, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 17, UserID = 9, Rating = 3, Review = "AVG", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 18, UserID = 10, Rating = 4, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 19, UserID = 11, Rating = 1, Review = "WORST", IsLikeFields = false });
+        list.Add(new ProductDetails { ProductID = 20, UserID = 12, Rating = 2, Review = "BELOW_AVG", IsLikeFields = false });
+        list.Add(new ProductDetails { ProductID = 21, UserID = 13, Rating = 1, Review = "WORST", IsLikeFields = false });
+        list.Add(new ProductDetails { ProductID = 22, UserID = 19, Rating = 5, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 23, UserID = 11, Rating = 5, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 24, UserID = 12, Rating = 4, Review = "Good", IsLikeFields = true });
+        list.Add(new ProductDetails { ProductID = 25, UserID = 11, Rating = 5, Review = "Good", IsLikeFields = true });
+        Console.WriteLine(list.Count);
+    }
+}
